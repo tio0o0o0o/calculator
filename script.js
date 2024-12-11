d6dee7375160b6b7dca3cfee12ed6f35829bc0e4
edenisageden <taiyoperson@gmail.com>
2024-12-11 22:06:00 +1300

    feat: Create MVP

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // 1. Create function that takes in x, y, and operator, and executes the corresponding calculation
 function calculate(x, y, operator) {
+    x = Number(x);
+    y = Number(y);
     switch(operator) {
         case "add":
             return x + y;
@@ -16,12 +18,7 @@ function calculate(x, y, operator) {
     }
 }
 
-// 2. Create variables to store input data;
-let xInput = 0;
-let yInput = 0;
-let operatorInput = 0;
-
-// 3. Create functions to get and append input from display
+// 2. Create functions for the buttons
 const display = document.querySelector("#display");
 
 function getInput() {
@@ -43,13 +40,41 @@ function appendZero() {
     if (display.textContent !== "0") display.textContent += "0";
 }
 
+function deleteLast() {
+    if (display.textContent.length === 1) display.textContent = 0;
+    else display.textContent = display.textContent.slice(0, -1);
+}
+
+function deleteAll() {
+    display.textContent = 0;
+}
+
 // Check if point doesn't exists in display and append
 function appendPoint() {
     if (display.textContent === "") display.textContent += "0.";
     else if (!display.textContent.includes(".")) display.textContent += ".";
 }
 
-// 4. Add event listeners to buttons
+let xInput = "";
+let yInput = "";
+let operatorInput = "";
+
+function onOperatorButton(operator) {
+    xInput = display.textContent;
+    display.textContent = "";
+    operatorInput = operator;
+}
+
+function onEqualsButton() {
+    if (xInput !== "" && operatorInput !== "" && display.textContent !== "") {
+        let answer = calculate(xInput, display.textContent, operatorInput);
+        display.textContent = answer;
+        xInput = "";
+        operatorInput = "";
+    }
+}
+
+// 3. Add event listeners to buttons
 const calculator = document.querySelector("#calculator");
 
 calculator.addEventListener("click", (event) => {
@@ -86,6 +111,30 @@ calculator.addEventListener("click", (event) => {
             break;
         case "point": 
             appendPoint();
+            break;
+        case "c":   
+            deleteLast();
+            break;
+        case "ac":
+            deleteAll();
+            break;
+        case "add":
+            onOperatorButton("add");
+            break;
+        case "subtract":
+            onOperatorButton("subtract");
+            break;
+        case "multiply":
+            onOperatorButton("multiply");
+            break;
+        case "divide":
+            onOperatorButton("divide");
+            break;
+        case "modulo":
+            onOperatorButton("modulo");
+            break;
+        case "equals":
+            onEqualsButton();
             break;
     }
 });
